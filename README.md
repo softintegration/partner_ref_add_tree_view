2a7807fc9d00ed7cd28f3b72059a1ad37bbe11ea
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-01-01 23:07:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Partner reference in tree view
+--------------------------------
+This module add the reference of partner in the tree view.
+
+
---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Partner reference in tree view',
+    'version': '1.0.1',
+    'author':'Soft-integration',
+    'category': 'Sale',
+    'summary': 'Partner reference in tree view',
+    'description': "",
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'views/res_partner_views.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
